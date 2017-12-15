b527757888d3e9f7153cae43d26b35e4b0c869c9
oliverke <oliver.ke@prismtech.com>
2017-12-15 10:44:02 +0800

    add SHA repeat mechanism for seed generate.

--- IOTA_Seed_Generator.py
+++ IOTA_Seed_Generator.py
@@ -7,11 +7,23 @@ cmd = raw_input(">>> Input string(0 for random):")
 
 seed = ""
 i = 0
+#enter = raw_input("input enter:")
+#print enter,",strlen of enter:",len(enter)
 if cmd=="0":
     for i in range(0,81):
         seed += str( source[random.randint(0,26)])
 else:
-    tmp = cheap_hash(cmd)
+    repeat = raw_input(">>> SHA time(enter for 1 time):")
+    if len(repeat) == 0:
+        i_repeat = 1
+    else:
+        try:
+            i_repeat = int(repeat)
+        except ValueError:
+            print ">>> Error input, SHA only 1 time."
+            i_repeat = 1
+    for i in range(0,i_repeat):
+        cmd = cheap_hash(cmd)
     for i in range(0,81):
-        seed += str( source[ord(tmp[i])%27])
-print seed," stringlen:",len(seed)
+        seed += str( source[ord(cmd[i])%27])
+print ">>> SEED:",seed," Stringlen:",len(seed)
